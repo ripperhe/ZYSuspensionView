acb887b7649ce2ada0b9c4eb7c911b4842ad2f85
ripper <ripperhe@qq.com>
2017-06-02 16:59:51 +0800

    删除多余的方法名

--- ZYSuspensionView/ZYSuspensionView/NSObject+ZYSuspensionView.m
+++ ZYSuspensionView/ZYSuspensionView/NSObject+ZYSuspensionView.m
@@ -18,24 +18,20 @@ const void *zy_suspensionMd5Key = &zy_suspensionMd5Key;
 {
     NSString *str = objc_getAssociatedObject(self, zy_suspensionMd5Key);
     if (str.length <= 0) {
-        str = [self getCurrentMd5Key];
+        NSString *desStr = self.description;
+        const char *cStr = [desStr UTF8String];
+        unsigned char result[16];
+        CC_MD5( cStr, (int)strlen(cStr), result );
+        str = [NSString stringWithFormat:
+               @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+               result[0],result[1],result[2],result[3],
+               result[4],result[5],result[6],result[7],
+               result[8],result[9],result[10],result[11],
+               result[12],result[13],result[14],result[15]];
+        
         objc_setAssociatedObject(self, zy_suspensionMd5Key, str, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }
     return str;
-}
-
-- (NSString *)getCurrentMd5Key
-{
-    NSString *str = self.description;
-    const char *cStr = [str UTF8String];
-    unsigned char result[16];
-    CC_MD5( cStr, (int)strlen(cStr), result );
-    return [NSString stringWithFormat:
-            @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
-            result[0],result[1],result[2],result[3],
-            result[4],result[5],result[6],result[7],
-            result[8],result[9],result[10],result[11],
-            result[12],result[13],result[14],result[15]];
 }
 
 @end
